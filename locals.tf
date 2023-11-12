887f383375009488aa5486d75f8f446cc8e11924
flagscript <flagscript@gmail.com>
2023-11-12 10:00:11 -0700

    feat(5): Remove application id

--- locals.tf
+++ locals.tf
@@ -10,12 +10,6 @@ locals {
   current_account_id = data.aws_caller_identity.current.account_id
   current_region     = data.aws_region.current.id
   common_tags = {
-    # "flagscript:account-owner:owner"            = var.org_owner
-    # "flagscript:automation:environment"         = "root"
-    "flagscript:cost-allocation:application-id" = var.application_id
-    # "flagscript:cost-allocation:owner"          = var.org_owner
-    # "flagscript:data-owner:owner"               = var.org_owner
-    # "flagscript:operations:owner"               = var.org_owner
     "github:module:repository" = "flagscript/terraform-aws-flagscript-s3-bucket"
     "terraform:module"         = "terraform-aws-flagscript-s3-bucket"
   }
